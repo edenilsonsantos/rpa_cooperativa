f3b54246d47edad2f1621c69a6e351a7c8d3d108
Edenilson F. Santos <47802746+edenilsonsantos@users.noreply.github.com>
2023-06-01 14:10:37 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -1,26 +1,13 @@
 
-    
-    
-from rpa_coop import dados, gerador_de_codigo
+########################################
 
-#gerador_de_codigo.template_novo_rpa(__file__)
+from rpa_coop import acc
 
+# usando multi janelas do sistema acc, alternando entre elas
+acc.open_acclient(['siat', 'sacg'])
 
-def main(processo=0):
-    qtd_atividades = 1
-    msg_log = "em andamento"
-    update_status = "andamento"
-    try:
+acc.exist_text('Retorna ao Sistema', nome_janela = 'siat')
+acc.select_menu_letras('cbaa', nome_janela = 'siat')
 
-        # SEU CODIGO AQUI 
-
-        qtd_atividades = 1
-        msg_log = "Concluido com sucesso"
-        update_status = "sucesso"
-    except Exception as e:
-        msg_log = str(e)
-        update_status = "erro"
-        qtd_atividades = 1
-    finally:
-        qtd_atividades = str(qtd_atividades)
-        return update_status, msg_log, qtd_atividades
+acc.exist_text('Retorna ao Sistema', nome_janela = 'sacg')
+acc.select_menu_letras('cga', nome_janela = 'sacg')
